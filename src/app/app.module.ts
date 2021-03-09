67ddb4ae0bb27fdb3f17693aa50c98ecc054a0a3
ryanl13214 <43986756+ryanl13214@users.noreply.github.com>
2021-03-09 21:32:57 +0000

    basic list design complete
    added in the basic list cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// pages
 import { HomepageComponent } from './homepage/homepage.component';
+
+// material
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,8 @@ import { HomepageComponent } from './homepage/homepage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+currPage:Number= 0;
+items:any =[{
+				"customer_name": "ABC Joinery",
+				"customer_address_street": "12 Ascott Street",
+				"customer_address_city": "Dundee",
+				"premium": 123.87,
+				"policy_type": "Public Liability",
+				"insurer_name": "Aviva"
+			},
+			{
+				"customer_name": "XYZ Plumbing",
+				"customer_address_street": "24 Fleet Street",
+				"customer_address_city": "Glasgow",
+				"premium": 2321.45,
+				"policy_type": "Public Liability",
+				"insurer_name": "Allianz"
+			},
+			{
+				"customer_name": "Fast Taxis",
+				"customer_address_street": "324b Bank Street",
+				"customer_address_city": "Aberdeen",
+				"premium": 59897.00,
+				"policy_type": "Motor Fleet",
+				"insurer_name": "Aviva"
+			},
+			{
+				"customer_name": "Fast Taxis",
+			 	"customer_address_street": "324b Bank Street",
+				"customer_address_city": "Aberdeen",
+				"policy_type": "Public Liability",
+			  "premium": 6845.00,
+				"insurer_name": "QBE"
+			}
+		];
 
+    // for use in the lsit list controls how many items are displayed at once
+    arrayOne(n: number): any[] {
+      return Array(n);
+    }
+
+  constructor() { }
+  public screenWidth: any;
+  public screenHeight: any;
   ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
   }
 
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
 }
