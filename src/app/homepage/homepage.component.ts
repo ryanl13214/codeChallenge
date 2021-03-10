aba3d462347ff04511dcde9bba95323c3e0cccd2
ryanl13214 <43986756+ryanl13214@users.noreply.github.com>
2021-03-10 11:37:10 +0000

    added in basic inserts
    can now do basic table insertion. somewhat buggy atm

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -41,6 +41,29 @@ CitySort: string = '';
     City: string = '';
 
 
+
+// for adding new items
+NewCustomer: any = {
+Customer_Name:"",
+Customer_street_address:"",
+Customer_city:""
+};
+
+NewPolicy: any = {
+premium:0,
+Customer_Customer_Name:"",
+Insurer_Insurer_Name:"" ,
+Client_Client_Name:"Achme Broker Ltd",
+policy_type_policy_type:""
+};
+
+newPolicy_type: string = '';
+
+newInsurer: string = " sdfsdf";
+
+
+
+
     // for use in the lsit list controls how many items are displayed at once max of 4 but
     // at the end of the list displays less to make it clearer when the list eeds.
     arrayOne(n: number): any[]
@@ -102,7 +125,37 @@ CitySort: string = '';
             var mainDisplay = document.getElementById("net").style.display = "inline-block";
         }
     }
+
+
+
+
+SavePolicyType(){
+  console.log("SavePolicyType");
+  this.sqlapi.SavePolicyType(this.newPolicy_type );
+}
+SaveCustomer(){
+    console.log("SaveCustomer");
+  this.sqlapi.SaveCustomer(this.NewCustomer.Customer_Name ,   this.NewCustomer.Customer_street_address ,     this.NewCustomer.Customer_city      );
+}
+
+SavePolicy(){
+    console.log("SavePolicy");
+  this.sqlapi.SavePolicy(this.NewPolicy.premium,this.NewPolicy.Customer_Customer_Name,this.NewPolicy.Insurer_Insurer_Name,this.NewPolicy.Client_Client_Name,this.NewPolicy.policy_type_policy_type);
+}
+SaveInsurer(){
+    console.log("SaveInsurer");
+  this.sqlapi.SaveInsurer(this.newInsurer);
+}
+
+
+
+
+
+
+
+
     sortCity()
+
     {
         if(this.CitySort == '')
         {
@@ -171,9 +224,19 @@ CitySort: string = '';
 
     sortByAlphabet(direction)
     {
+      if(direction == "asc")
+      {
+
       this.items.sort(function (a, b) {
        return a.customer_address_city.length - b.customer_address_city.length;
      });
+}else{
+  this.items.sort(function (a, b) {
+   return b.customer_address_city.length - a.customer_address_city.length;
+ });
+}
+
+
 
    console.log(  this.items);
     }
--- src/app/sqlapi.service.ts
+++ src/app/sqlapi.service.ts
@@ -52,4 +52,63 @@ updatePolicy(pk,customer_Name,policy_type,insurer_Name,premium,customer_address_
 
 
 
+
+
+
+SavePolicyType(newPolicy_type){
+  var temp = this.baseUrl+"/SavePolicyType.php?newPolicy_type="+newPolicy_type;
+
+      return this.http.get(temp).pipe(
+        map((res) => {
+         return res['data'];
+      }));
+}
+
+
+
+SaveCustomer(customer_Name , customer_street_address , customer_city){
+  var temp = this.baseUrl+"/SaveCustomer.php?customer_Name="+customer_Name+ "&customer_address_street="+customer_street_address+ "&customer_address_city="+customer_city   ;
+
+      return this.http.get(temp).pipe(
+        map((res) => {
+         return res['data'];
+      }));
+}
+
+
+
+
+
+SavePolicy(premium , customer_Customer_Name , insurer_Insurer_Name , client_Client_Name, policy_type_policy_type){
+  var temp = this.baseUrl+"/SavePolicy.php?premium="+premium+ "&customer_Customer_Name="+customer_Customer_Name+ "&insurer_Insurer_Name="+insurer_Insurer_Name+ "&client_Client_Name="+client_Client_Name+ "&policy_type_policy_type="+policy_type_policy_type;
+
+      return this.http.get(temp).pipe(
+        map((res) => {
+         return res['data'];
+      }));
+}
+SaveInsurer(newInsurer){
+  console.log(this.baseUrl+"/SaveInsurer.php?newInsurer="+newInsurer);
+  var temp =  this.baseUrl+"/SaveInsurer.php?newInsurer="+newInsurer;
+
+      return this.http.get(temp).pipe(
+        map((res) => {
+         return res['data'];
+      }));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
